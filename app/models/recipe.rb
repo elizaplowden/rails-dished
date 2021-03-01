31c70cae5a26a60a5f9a31682980423d614832fa
Eliza Plowden <elizaplowden@gmail.com>
2021-03-01 17:38:53 +0000

    edited Recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :reviews, :bookmarks, dependent: :destroy
   has many :ingredients
-  validates :name, :description, :instructions, :serves, :cook_time, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :description, :instructions, :serves, :cook_time, presence: true
   # has_many_attached :photos
 end
