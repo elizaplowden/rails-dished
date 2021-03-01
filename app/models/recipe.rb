948a4d4537e3e34455d3efaef3ec3f4141b03f27
Eliza Plowden <elizaplowden@gmail.com>
2021-03-01 17:34:32 +0000

    added validations and associations to Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :reviews, :bookmarks, dependent: :destroy
+  has many :ingredients
+  validates :name, :description, :instructions, :serves, :cook_time, presence: true
+  # has_many_attached :photos
 end
