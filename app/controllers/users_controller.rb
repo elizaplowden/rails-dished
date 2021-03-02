cfbd1754e4d3c4ca3ba90e59135f97100c83d753
Eliza Plowden <elizaplowden@gmail.com>
2021-03-02 14:45:51 +0000

    added follow links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,28 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :follow, :unfollow]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def follow
-    @user = User.find(params[:id])
     Following.create(follower: current_user, followee: @user)
     redirect_to user_path(@user)
   end
 
   def unfollow
-    @user = User.find(params[:id])
     Following.find_by(follower: current_user, followee: @user).destroy
     redirect_to users_path
   end
 
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 
 
 end
