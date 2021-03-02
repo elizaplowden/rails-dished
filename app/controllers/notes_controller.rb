75cf84ea5cd88cf20a35abc4b55fbc56f9888a6b
Eliza Plowden <elizaplowden@gmail.com>
2021-03-02 16:36:48 +0000

    create note action

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,29 @@
 class NotesController < ApplicationController
+  before_action :find_bookmark, only: [:new, :create]
+
   def new
     @note = Note.new
   end
+
+  def create
+    @note = Note.new(strong_params)
+    @note.bookmark = @bookmark
+    @note.user = current_user
+    if @note.save
+      redirect_to bookmark_path(@bookmark.id)
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:note).permit(:description)
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
 end
