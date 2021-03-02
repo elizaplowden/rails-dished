fd892fe274bb0a7a878aedac430a3986a439aa0f
Sara Evans <saraevs@gmail.com>
2021-03-02 12:11:42 +0000

    deleted all existing records and created 2 new users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'deleting existing records...'
+Cookbook.delete_all
+Bookmark.delete_all
+Following.delete_all
+Food.delete_all
+Ingredient.delete_all
+Note.delete_all
+Recipe.delete_all
+Review.delete_all
+User.delete_all
+
+
+puts 'creating users...'
+
+User.create(email: 'email@gmail.com', password: 'Password1')
+User.create(email: 'email@hotmail.com', password: 'Password2')
+
+puts "created #{User.count} users"
+
+
