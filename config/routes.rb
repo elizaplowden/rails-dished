93037b958a8c694a5fcb16bcb9284648397729b4
Sara Evans <saraevs@gmail.com>
2021-03-02 11:21:12 +0000

    added basic resources to routes and validation to bookmark model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cookbook do
+    resources :bookmarks, only [ :new, :create ]
+  end
+
+  resources :recipe do
+    resources :ingredients, only [ :new, :create ]
+    resources :reviews, only [ :new, :create ]
+  end
+
+  resources :bookmarks do
+    resources :notes, only [ :new, :create ]
+  end
+
+  resources :notes, only [ :destroy ]
 end
