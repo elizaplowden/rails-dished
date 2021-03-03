833af62a99d6e6efad5fc77e510842a26fda4052
Phillip Gardner <pbg.gardner@gmail.com>
2021-03-03 14:56:04 +0000

    remove changes to js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery
-//= require popper
-//= require turbolinks
-//= require bootstrap
-//= require_tree.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
