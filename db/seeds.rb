efc994707e125f6d2fb62cbb5096fdcc442258d5
Sara Evans <saraevs@gmail.com>
2021-03-04 11:01:00 +0000

    added some logic to the wishlist modal to ignore if the recipe id doesn't exist in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ require 'open-uri'
 
 puts 'deleting existing records...'
 
-Note.delete_all
+#  remove the session before destroying all the records
+# session = nil
+Note.destroy_all
 Review.delete_all
 Bookmark.delete_all
 Ingredient.delete_all
@@ -40,7 +42,7 @@ puts "created #{Food.count} foods"
 
 
 # seeding some recipes from meals.db api
-
+puts 'creating recipes, ingredients and photos...'
 10.times do
 
   recipes_url = 'https://www.themealdb.com/api/json/v1/1/random.php'
@@ -59,6 +61,7 @@ puts "created #{Food.count} foods"
       user: User.first,
     } )
      # creating arrays of the ingredients and foods
+     # only adds the first 5 ingredients of 20
     foods = [meal['strIngredient1'], meal['strIngredient2'], meal['strIngredient3'], meal['strIngredient4'], meal['strIngredient5']]
     measures = [meal['strMeasure1'], meal['strMeasure2'], meal['strMeasure3'], meal['strMeasure4'], meal['strMeasure5']]
     # iterating over each measure
@@ -67,17 +70,15 @@ puts "created #{Food.count} foods"
       food = Food.find_by(name: foods[index])
       # creating the ingredient instances
       Ingredient.create({
-      recipe: recipe,
-      food: food,
-      quantity: measure
-     })
-
-     # attaching a photo to the recipe
-     image_url = meal['strMealThumb']
-     downloaded_image = open(image_url)
-     filename = File.basename(image_url)
-     recipe.photo.attach(io: downloaded_image, filename: filename)
-     puts 'attached photo'
+        recipe: recipe,
+        food: food,
+        quantity: measure
+      })
+      # attaching photos to the recipes
+      image_url = meal['strMealThumb']
+      downloaded_image = open(image_url)
+      filename = File.basename(image_url)
+      recipe.photo.attach(io: downloaded_image, filename: filename)
     end
   end
 end
