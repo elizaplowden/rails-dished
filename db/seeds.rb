0ad3dd1aa1650d7f0192377ab7c2d705c4be106b
Eliza Plowden <elizaplowden@gmail.com>
2021-03-05 12:47:16 +0000

    landing page edits but was in wrong branch!

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,6 +56,32 @@ recipes_url = [
   'https://themealdb.com/api/json/v1/1/search.php?s=teriyaki',
   'https://themealdb.com/api/json/v1/1/search.php?s=tagine'
 ]
+image_url = [
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944353/curry_enmkfv.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944736/pancakes_qrqfsm.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944817/photo-1473093295043-cdd812d0e601_feew13.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944885/burger_v3wcmm.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945126/photo-1579523609100-5b868b803668_z4psfa.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945367/tacos_yjk701.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945552/apple_tart_fqencu.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945687/quinoa_n9zmji.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944557/salmon_xv79pu.jpg',
+      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945819/tagine_asyowg.jpg'
+]
+
+image_name = [
+      'curry_enmkfv.jpg',
+      'pancakes_qrqfsm.jpg',
+      'photo-1473093295043-cdd812d0e601_feew13.jpg',
+      'burger_v3wcmm.jpg',
+      'photo-1579523609100-5b868b803668_z4psfa.jpg',
+      'tacos_yjk701.jpg',
+      'apple_tart_fqencu.jpg',
+      'quinoa_n9zmji.jpg',
+      'salmon_xv79pu.jpg',
+      'tagine_asyowg.jpg'
+]
+
 
 10.times do
   recipes_serialized = open(recipes_url[counter]).read
@@ -87,30 +113,6 @@ recipes_url = [
      })
     end
      # attaching a photo to the recipe
-     image_url = [
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944353/curry_enmkfv.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944736/pancakes_qrqfsm.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944817/photo-1473093295043-cdd812d0e601_feew13.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944885/burger_v3wcmm.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945126/photo-1579523609100-5b868b803668_z4psfa.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945367/tacos_yjk701.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945552/apple_tart_fqencu.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945687/quinoa_n9zmji.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614944557/salmon_xv79pu.jpg',
-      'https://res.cloudinary.com/dupmc3vsd/image/upload/v1614945819/tagine_asyowg.jpg']
-     image_name = [
-      'curry_enmkfv.jpg',
-      'pancakes_qrqfsm.jpg',
-      'photo-1473093295043-cdd812d0e601_feew13.jpg',
-      'burger_v3wcmm.jpg',
-      'photo-1579523609100-5b868b803668_z4psfa.jpg',
-      'tacos_yjk701.jpg',
-      'apple_tart_fqencu.jpg',
-      'quinoa_n9zmji.jpg',
-      'salmon_xv79pu.jpg',
-      'tagine_asyowg.jpg'
-       ]
-
      downloaded_image = open(image_url[counter])
      filename = image_name[counter]
      recipe.photo.attach(io: downloaded_image, filename: filename)
