a21b82218063e8e11da0a1057614fdcbdbd13490
Sara Evans <saraevs@gmail.com>
2021-03-08 18:21:58 +0000

    navbar background changes to white when you scroll past the banner image

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,6 +2,9 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
+      // console.log('scrolling');
+      // console.log(`scrollY: ${window.scrollY}`);
+      // console.log(`inner height: ${window.innerHeight}`);
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
