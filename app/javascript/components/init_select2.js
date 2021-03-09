b88ed0f5962af1fb3e65b2e53805f427162e6c08
Sara Evans <saraevs@gmail.com>
2021-03-09 14:18:29 +0000

    added some styling to override select2 and added some logic to the controller to account for no query strings

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,14 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+      placeholder: "What's in your fridge?"
+      // theme: "flat"
+  });
 };
+
+// $(".js-example-theme-multiple").select2({
+//   theme: "classic"
+// });
 
 export { initSelect2 };
