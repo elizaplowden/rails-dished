9a8194bb8ca5367092e3873045c136f2ce6d677d
Sara Evans <saraevs@gmail.com>
2021-03-09 15:04:58 +0000

    finished styling on the home page search bar

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,10 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2({
-      placeholder: "What's in your fridge?"
-      // theme: "flat"
-  });
+  $('.select2').select2();
 };
 
 // $(".js-example-theme-multiple").select2({
