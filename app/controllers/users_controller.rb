5752ae8419f58fb0119fefb85c7b85ddbf28f11d
Eliza Plowden <elizaplowden@gmail.com>
2021-03-10 09:26:48 +0000

    removed average ratings again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show, :follow, :unfollow]
-  # before_action :average_rating, only: :index
 
   def index
     @users = User.where.not(id: current_user.id)
@@ -18,18 +17,6 @@ class UsersController < ApplicationController
     Following.find_by(follower: current_user, followee: @user).destroy
     redirect_back(fallback_location: users_path)
   end
-
-  # def average_rating
-  #   @recipe = Recipe.new
-  #   reviews = @recipe.reviews
-  #   ratings = []
-  #   reviews.each do |review|
-  #     ratings << review.rating
-  #   end
-  #   unless ratings.empty?
-  #     @average_rating = (ratings.sum / ratings.size)
-  #   end
-  # end
 
   private
 
