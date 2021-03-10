78726313e3d284c111804d016b28c56e4a41023c
Sara Evans <saraevs@gmail.com>
2021-03-10 12:06:39 +0000

    added placeholder to search box

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,9 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    placeholder: "What's in your fridge?"
+  });
 };
 
 // $(".js-example-theme-multiple").select2({
