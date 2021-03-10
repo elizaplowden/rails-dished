342181dce67d5c77500527f8d724343ce2547e0c
Eliza Plowden <elizaplowden@gmail.com>
2021-03-10 16:27:29 +0000

    added comments to jquery stars code

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -4,8 +4,11 @@ import "jquery-bar-rating";
 // import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
 
 const initStarRating = () => {
+  // to enable the stars. ID found by using inspector
   $('#review_rating').barrating({
+    // passing the object to the method to select the jquery theme we want
     theme: 'css-stars',
+    // submit the form automatically when stars are selected
     onSelect: (value, text, event) => {
       const form = $("form.review_form"); //selecting the form on the page with this class
       form.submit(); // Submitting the form with JS
