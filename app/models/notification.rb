c1fa6764128602839ef0d3ebcaf9c5dc1c648f0c
Phillip Gardner <pbg.gardner@gmail.com>
2021-03-10 10:34:08 +0000

    adding notification index action

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
+
+  scope :unread, -> { where(read_at: nil) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  resources :notifications
 
   resources :recipes do
     resources :ingredients, only: :create
